5c3fc491078c523e115c89fa2721810f6533bd63
m3cj <114652316+m3cj@users.noreply.github.com>
2024-01-02 02:38:40 +0530

    redirect testing

--- script/script.js
+++ script/script.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the category button element
+    const hots = document.getElementById("hots");
+    const spots = document.getElementById("spots");
+  
+    hots.addEventListener("click", function () {
+      window.location.href = '/pages/events.html';
+    });
+
+    spots.addEventListener("click", function () {
+        window.location.href = '/pages/category.html';
+      });
+  });
