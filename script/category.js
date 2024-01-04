e2438caaf8c412723cee22005df10d651b3eae11
m3cj <114652316+m3cj@users.noreply.github.com>
2024-01-04 15:56:58 +0530

    search function on category

--- script/category.js
+++ script/category.js
@@ -1,4 +1,24 @@
-const img = document.getElementById('catImg')
-img.addEventListener("click", function () {
-    window.location.href = './rankingList.html';
+let card = document.querySelector(".card")
+let cardImg = card.querySelector("img")
+
+card.addEventListener("click", () => {
+    window.location.href = './rankingList.html'
 })
+
+function searchFuncCat() {
+    let input = document.getElementById("inputText")
+    let toSearch = input.value.toUpperCase()
+    let list = document.querySelector("#mainList")
+    let innerCard = document.querySelectorAll(".card")
+
+    for (let i = 0; i < innerCard.length; i++) {
+        let headings = innerCard[i].getElementsByTagName("h1")
+        let actualText = headings[0].textContent.toUpperCase();
+
+        if (actualText.indexOf(toSearch) > -1) {
+            innerCard[i].style.display = ""
+        } else {
+            innerCard[i].style.display = "none"
+        }
+    }
+}
